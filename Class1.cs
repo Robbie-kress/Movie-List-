4f5418f7dfed6c01884f2bcaef0f52709704d284
Robbie-kress <68262512+Robbie-kress@users.noreply.github.com>
2020-07-31 08:50:12 -0400

    Movie List Lab

---
+++ Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace MovieInList
+{
+    class MovieProgram
+    {
+        #region fields                  
+        private string movieTitles;
+        private string catagories;
+        private int starRating;
+        private DateTime releaseDate;
+        private bool tomatoApproved;
+
+        #endregion
+
+        #region Properties 
+        //non static. accessible outside of this class
+        public string MovieTitles
+        {
+            get { return movieTitles; } //retrieve value of our property through field
+            set { movieTitles = value; }//set the value of the property through field
+        }
+        public string Catagories
+        {
+            get { return catagories; }
+            set { catagories = value; }
+        }
+        public int StarRating
+        {
+            get { return starRating; }
+            set { starRating = value; }
+        }
+        public DateTime ReleaseDate
+        {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+        public bool ToSee
+        {
+            get { return tomatoApproved; }
+            set { tomatoApproved = value; }
+        } 
+        // end of all properties stated
+        #endregion
+
+        #region contructors
+        public MovieProgram() { } // default constructor
+
+
+                                //overloaded constructor
+        public MovieProgram(string _movieTitles, string _catagories, int _starRating, DateTime _releaseDate, bool _toSee) //private string = use underscore
+        {
+            movieTitles = _movieTitles;
+            catagories = _catagories;
+            starRating = _starRating;
+            releaseDate = _releaseDate;
+            tomatoApproved = _toSee;
+
+        }
+        #endregion
+
+        #region Methods
+        public void PrintCatagory() //print out information to console
+        {
+
+
+
+            if (tomatoApproved)
+            {
+                Console.WriteLine($"{movieTitles} is a great choice! That movie is a {catagories}. That movie recieved a {starRating} on SmellyTomato's.com, you can trust us we know movies. This movie released in {releaseDate} is available now for streaming or purchase at WhobuysMovies.com");
+               
+                
+            }
+            else
+            {
+                Console.WriteLine($"{movieTitles} is a great choice! That movie is a {catagories}. That movie recieved a {starRating} on SmellyTomato's.com, you can trust us we know movies. This movie released in {releaseDate} is available now for streaming or purchase at WhobuysMovies.com");
+
+            }
+        } 
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace MovieInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<MovieProgram> movieList = new List<MovieProgram>();
+            {
+
+                new MovieProgram("Children of the corn", "horror", 4, DateTime.Parse("03/09/1984"), true);
+                new  MovieProgram ("The Notebook", "anime", 2, DateTime.Parse("12/25/1978"), true);
+                new MovieProgram("Butterfly Candle", "drama", 3, DateTime.Parse("11/11/1111"), true);
+                new MovieProgram("Strangers", "scifi", 5, DateTime.Parse("09/23/2082"), true);
+                new MovieProgram("I'm tired", "drama", 4, DateTime.Parse("7/30/2020"), true);
+                new MovieProgram("SuperDan", "horror", 2, DateTime.Parse("04/29/2012"), true);
+                new MovieProgram("Clapping", "scifi", 1, DateTime.Parse("03/21/1993"), true);
+            }
+
+            Console.WriteLine("Welcome to SmellyTomato's movie list app!");
+            Console.WriteLine("\n=======================================\n");
+            Console.WriteLine("What movie catagory are you interested in?");
+            string movie1 = Console.ReadLine();
+            
+            Console.WriteLine();
+            Continue();
+
+            movieList.Add(new MovieProgram());
+            
+            foreach (MovieProgram movie in movieList)
+            {
+                
+                if (movie.MovieTitles == "horror")
+                {
+                    movie.PrintCatagory();
+                }
+                else
+                {
+
+                }
+                if(movie.MovieTitles == "anime")
+                {
+                    movie.PrintCatagory();
+                }
+                else
+                {
+
+                }
+                if(movie.MovieTitles == "scifi")
+                {
+                    movie.PrintCatagory();
+                }
+                else
+                {
+
+                }
+                if(movie.MovieTitles == "drama")
+                {
+                    movie.PrintCatagory();
+                }
+                
+                
+                
+            }
+
+            
+
+
+        }
+        public static bool Continue()
+        {
+            int counter = 3;
+
+            while (counter > 0)
+            {
+                Console.WriteLine("Would you like to continue [y/n].");
+                string input = Console.ReadLine().ToLower().Trim();
+
+                if (input.StartsWith('y'))
+                {
+                    return true;
+                }
+                else if (input == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    counter--;
+                    Console.WriteLine($"You have {counter} tries left");
+
+                }
+
+                // System.Environment.Exit(0);
+            }
+
+            Console.WriteLine("Oh no you didn't do it right!");
+            return false;
+        }
+
+
+
+    }
+}
